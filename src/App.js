55ed47281eb2a535297d5430f4adaa9183cd6880
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-10-16 23:57:29 +0600

    Set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AddContact from './pages/AddContact/AddContact';
+import Home from './pages/Home/Home';
+import Footer from './pages/Shared/Footer';
+import Navbar from './pages/Shared/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route  path="/" element={<Home></Home>}/>
+        <Route path='/add' element={<AddContact></AddContact>}/>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/pages/Shared/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+            <footer className="items-center p-4 bg-neutral text-neutral-content text-center">
+                <div className="items-center grid-flow-col text-center"> 
+                    <p>Copyright © 2022 - All right reserved</p>
+                </div> 
+            </footer>
+    );
+};
+
+export default Footer;
